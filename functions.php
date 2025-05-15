89f0fbe8784fa1c836c2689d4f59ffd19906dad7
Ville Sundell <development@solarius.fi>
2025-05-15 20:02:27 +0300

    Initial commit of the theme
    Basic functionality, nothing fancy yet.

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Child theme functionality and smooth style enqueuing
+ */
+
+function greenchild_child_enqueue_styles() {
+  // Parent theme stylesheet
+  wp_enqueue_style(
+    'twenty-twenty-five-style',
+    get_template_directory_uri() . '/style.css',
+    array(),
+    wp_get_theme('twentytwentyfive')->get('Version')
+  );
+
+  // Child theme stylesheet
+  wp_enqueue_style(
+    'green-child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array('twenty-twenty-five-style'),
+    wp_get_theme()->get('Version')
+  );
+
+  // Google Fonts
+  wp_enqueue_style(
+    'greenchild-google-fonts',
+    'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&family=Barlow+Semi+Condensed:wght@400;600&display=swap',
+    array(),
+    null
+  );
+}
+add_action('wp_enqueue_scripts', 'greenchild_child_enqueue_styles');
+
